7d9f652433a1300657d4e08619d013552b8f5fb8
Arcy <73959934+arcy-at@users.noreply.github.com>
2021-10-06 16:08:30 -0500

    brain

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -1,30 +1,45 @@
-const { MessageEmbed, Message } = require("discord.js")
-module.exports.run = async (bot, message, args) => {
-  if (args[0]) {
-    const user = message.mentions.users.first();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.data = exports.run = void 0;
+const discord_js_1 = require("discord.js");
+async function findUser(client, context, query) {
+    query = query ?? context.author.id;
+    query = query.toLowerCase() === 'discord' ? '643945264868098049' : query;
+    return (client.users.cache.find((user) => {
+        return (user.username.toLowerCase().includes(query) ||
+            user.tag.toLowerCase().includes(query) ||
+            query.replace(/\D/g, "") == user.id);
+    }) ??
+        (await client.users.fetch(query.replace(/\D/g, ""), {
+            cache: true,
+            force: true,
+        })));
+}
+async function run(_bot, message, args) {
+    const user = await findUser(_bot, message, args[0] ?? "");
     if (!user) {
-      return message.reply({ content: 'Please use a proper mention if you want to see someone elses avatar.' });
+        return message.reply("❌ Can't find that user.");
     }
-    const embed = new MessageEmbed()
-      .setTitle(`${user.username}'s avatar:`)
-      .setImage(user.displayAvatarURL({ dynamic: true }))
-      .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-
+    const embed = new discord_js_1.MessageEmbed({
+        title: `Avatar of ${user.tag}`,
+        image: {
+            url: user.displayAvatarURL({ dynamic: true, size: 4096 })
+        },
+        color: 3092790,
+        footer: {
+            text: "Avatar",
+            iconURL: _bot.user?.displayAvatarURL({ size: 4096 })
+        }
+    });
     return message.channel.send({ embeds: [embed] });
-  }
-  const embed2 = new MessageEmbed()
-    .setTitle(`${message.author.username}'s avatar:`)
-    .setImage(message.author.displayAvatarURL({ dynamic: true }))
-    .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-  return message.channel.send({ embeds: [embed2] });
 }
-
-module.exports.data = {
-  name: "avatar",
-  description: "Displays another user's or your avatar",
-  aliases: ["av", "pfp"],
-  category: "Info",
-  requires: "none",
-  usage: ["avatar [mention]"],
-  examples: ["avatar", "avatar @GangstaCat"]
-}
+exports.run = run;
+exports.data = {
+    name: "avatar",
+    description: "Displays another user's or your avatar",
+    aliases: ["av", "pfp"],
+    category: "Info",
+    requires: "none",
+    usage: ["avatar [user]"],
+    examples: ["avatar", "avatar @GangstaCat", "avatar discord"],
+};
