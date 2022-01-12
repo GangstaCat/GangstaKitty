41e1c62f529b185cddc766f388de488579f98e80
GangstaCat <rik.audenaert@outlook.com>
2022-01-12 17:29:04 +0100

    Updated and deleted commands from the info section

--- commands/Info/devinfo.js
+++ commands/Info/devinfo.js
@@ -6,10 +6,9 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor('GangstaKitty')
     .setDescription('Info and socials about GangstaCat, the developer of this bot')
     .setThumbnail("https://cdn.discordapp.com/attachments/856832353409302538/857961784443142174/petgangsta.gif")
-    .addField('General info', "Hi, i'm GangstaCat. I enjoy doing things like making simple digital art, programming and more. I have my own server with about 60 members and i also moderate a bunch of servers")
+    .addField('General info', "I'm your just not so average 13 year old programmer who just so happens to have made this bot")
     .addField('Youtube', "Subscribe to GangstaCat here: https://www.youtube.com/channel/UCi-mEQw6JdB81cRQ9NKuY2w")
     .addField('Twitch', "Follow GangstaCat on Twitch: https://twitch.tv/gangstacatttv")
-    .addField('Twitter', "Follow GangstaCat on Twitter: https://twitter.com/gangstacatgd")
     .setFooter(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }));
 
   message.channel.send({ embeds: [creatorEmbed] });
