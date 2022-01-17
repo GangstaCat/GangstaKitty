7b0ce046ecf44678a3d845e99feef1878ea8b16f
GangstaCat <rik.audenaert@outlook.com>
2022-01-17 11:20:51 +0100

    improved and moved timeout command

--- commands/Moderation/timeout.js
+++ commands/Moderation/timeout.js
@@ -2,10 +2,13 @@ module.exports.run = async (bot, message, args) => {
   if (message.member.permissions.has("TIMEOUT_MEMBERS")) {
     const target = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
 
+    let timeoutReason = args.slice(2).join(" ");
     if (target) {
+      if (!timeoutReason) {
+        timeoutReason = "Unspecified";
+      }
       const memberTarget = message.guild.members.cache.get(target.id);
       const timeoutTime = Number(args[1]);
-      const timeoutReason = args.slice(2).join(" ");
 
       memberTarget.timeout(timeoutTime, timeoutReason)
       message.channel.send({ content: `${message.author} timed out <@${memberTarget.id}> (\`${memberTarget.id}\`) for ${timeoutTime} milliseconds. Reason: ${timeoutReason}` });
@@ -21,7 +24,7 @@ module.exports.data = {
   name: "timeout",
   description: "Timeout a member",
   aliases: "to",
-  category: "Experimental",
+  category: "Moderation",
   requires: "Permission: Timeout members",
   usage: "timeout <mention | id> <time (milliseconds)>",
   examples: "timeout @GangstaCat 60000"
