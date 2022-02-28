d54f480927135428f3aafe76801be6096111b046
GangstaCat <rik.audenaert@outlook.com>
2022-02-28 12:59:50 +0100

    added welcome messages

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 //variables
-const { Client, Intents, Collection } = require("discord.js");
+const { Client, Intents, Collection, MessageEmbed } = require("discord.js");
 const bot = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS
   ]
 });
 
 const prefix = '+';
-
-const { MessageActionRow, MessageButton } = require('discord.js');
 
 const fs = require('fs');
 
@@ -26,13 +25,23 @@ for (let folder of fs.readdirSync("./commands/")) {
     bot.commands.set(command.data.name, command);
     console.log(`(${++totalCommandCount}) ${categoryCount}-${++commandCount}: ${file}`);
   }
-  console.log("All commands loaded. Going online");
 }
 
 //online log and presence
 bot.on("ready", async () => {
   console.log('GangstaKitty is online!');
   bot.user.setActivity(`+help || eating code`, { type: 'LISTENING' })
+});
+
+bot.on("guildMemberAdd", guildMember => {
+  if (guildMember.guild.id === "914151815749181471") {
+    const embed = new MessageEmbed()
+      .setColor("007DFF")
+      .setTitle(`Welcome ${guildMember.user.tag}!`)
+      .setDescription("Welcome to this awesome server")
+      .setThumbnail(guildMember.user.displayAvatarURL({ dynamic: true }))
+    guildMember.guild.channels.cache.get("914151816386727948").send({ embeds: [embed] })
+  }
 });
 
 //command handler
@@ -46,56 +55,19 @@ bot.on("messageCreate", async message => {
     try {
       await command.run(bot, message, args);
     } catch (e) {
-      message.channel.send({ content: "an error occured while executing that command <a:confuzzled:860924268370722846>" })
+      message.channel.send({ content: "an error occured while executing that command" })
       console.error(`${message.guild.name} - #${message.channel.name} - ${message.channel.id}-${message.id}\n"${message.content}"\n`, e.stack);
     }
   }
-}
-
-  , bot.on("interactionCreate", async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-
-  })
-
-  //triggers
-  , bot.on("messageCreate", m => {
-    if (m.author.bot) return; if (m.content === "Gangsta") { m.channel.send({ content: "https://cdn.discordapp.com/attachments/856832353409302538/889431236693614642/GangstaCat.png" }) }
-  })
-  , bot.on("messageCreate", m => {
-    if (m.author.bot) return; if (m.content === "stfu") { m.channel.send({ content: "https://cdn.discordapp.com/attachments/553322925395017732/739837616014491698/video0.mp4" }) }
-  })
-
-  ,
-  //buttons
-
-  // bot.on('interactionCreate', async interaction => {
-  // 	if (!interaction.isCommand()) return;
-
-  // 	if (interaction.commandName === 'ping') {
-  // 		const row = new MessageActionRow()
-  // 			.addComponents(
-  // 				new MessageButton()
-  // 					.setCustomId('primary')
-  // 					.setLabel('Primary')
-  // 					.setStyle('PRIMARY'),
-  // 			);
-
-  // 		await interaction.reply({ content: 'Pong!', components: [row] });
-  // 	}
-  // }))
+});
 
 
-  //the bot login. crucial for getting the bot online
-  // bot.login(process.env.token))
-  bot.login(process.env.token))
+//triggers
+bot.on("messageCreate", m => {
+  if (m.author.bot) return; if (m.content === "Gangsta") { m.channel.send({ content: "https://cdn.discordapp.com/attachments/856832353409302538/889431236693614642/GangstaCat.png" }) }
+});
+bot.on("messageCreate", m => {
+  if (m.author.bot) return; if (m.content === "stfu") { m.channel.send({ content: "https://cdn.discordapp.com/attachments/553322925395017732/739837616014491698/video0.mp4" }) }
+});
+
+bot.login(process.env.TOKEN)
