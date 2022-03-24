c33c9e08f85cf6d91cd28fe610b372f562c366cc
GangstaCat <rik.audenaert@outlook.com>
2022-03-24 08:59:48 +0100

    why does this exist

--- commands/Experimental/startserver.js
+++ commands/Experimental/startserver.js
@@ -5,16 +5,6 @@ module.exports.run = async (bot, message, args) => {
   let server = args[0];
   if (!args.length) server = "GangstaCatTest.aternos.me";
 
-  const fs = require('fs');
-
-  fs.readdir("./aternosAPI", { withFileTypes: true }, (error, files) => {
-    const directoriesInDIrectory = files
-      .filter((item) => item.isDirectory())
-      .map((item) => item.name);
-
-    console.log(directoriesInDIrectory);
-  });
-
   process.chdir("aternosAPI");
   exec(`npm run start -- --${server}`, (error, stdout, stderr) => {
     if (error || stderr) {
