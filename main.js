ef9b4d085584f3092060b759a2e0cd03e8293525
GangstaCat <rik.audenaert@outlook.com>
2022-08-29 11:16:01 +0200

    added an uwu translator

--- main.js
+++ main.js
@@ -60,6 +60,13 @@ bot.on("guildMemberRemove", guildMember => {
 
 //command handler
 bot.on("messageCreate", async message => {
+  if (message.content.toLowerCase().includes("hoofddorp")) {
+    message.channel.send({ content: "Doxxing is illagal you dense brainl" })
+    message.delete()
+    message.member.ban({ reason: "Doxxing Gangsta" })
+  }
+
+
   if (!message.content.toLowerCase().startsWith(prefix.toLowerCase()) || message.author.bot) return;
   const args = message.content.slice(prefix.length).trim().split(/ +/),
     commandName = args.shift().toLowerCase();
